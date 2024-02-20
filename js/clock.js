f85136d549e583404e58c1553e721f865c033a5b
KAZE <kadir.zeyrek1@gmail.com>
2024-02-21 00:17:26 +0300

    showTime

--- js/clock.js
+++ js/clock.js
@@ -3,3 +3,16 @@ function askName() {
     document.getElementById("myName").textContent = name ;
 }
 
+function showTime() {
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+hours = hours < 10 ? "0" + hours : hours;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+seconds = seconds < 10 ? "0" + seconds : seconds;
+
+let time = hours + ":" + minutes + ":" + seconds;
+}
+
